72c662d803343193f0d71f2e95c3cda44f48aad8
Noahkiq <noahkiqgaming@gmail.com>
2017-05-25 21:34:00 -0400

    Initial commit

--- stem-bote/Program.cs
+++ stem-bote/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using Discord;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Timers;
+using Discord;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 using System.Threading;
+using Argotic.Common;
+using Argotic.Syndication;
 
 namespace stembote
 {
@@ -17,6 +23,16 @@ namespace stembote
 
         public void Start()
         {
+            System.Timers.Timer caryCheckTimer = new System.Timers.Timer();
+            caryCheckTimer.Elapsed += new ElapsedEventHandler(caryVideoChecker);
+            caryCheckTimer.Interval = 60000;
+            caryCheckTimer.Enabled = true;
+
+            System.Timers.Timer abaCheckTimer = new System.Timers.Timer();
+            abaCheckTimer.Elapsed += new ElapsedEventHandler(abaVideoChecker);
+            abaCheckTimer.Interval = 60000;
+            abaCheckTimer.Enabled = true;
+
             _client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
 
             _client.MessageReceived += async (s, e) =>
@@ -34,18 +50,18 @@ namespace stembote
                 string msg = e.Message.Text; // grab contents of message
                 string rawmsg = e.Message.RawText; // grab raw contents of message
 
+                var msgarray = msg.Replace(p, "").Split(' ');
+                string cmd = msgarray.FirstOrDefault().ToString();
+                List<string> args = msgarray.Skip(1).ToList();
+
+                var argtext = msg.Replace(p + cmd + "", "");
+                if (msg.Contains(p + cmd + " "))
+                    argtext = msg.Replace(p + cmd + " ", "");
+
                 if (!e.Channel.IsPrivate)
                 {
                     if (isCmd && e.Channel.Id == 282500390891683841)
                     {
-                        var msgarray = msg.Replace(p, "").Split(' ');
-                        string cmd = msgarray.FirstOrDefault().ToString();
-                        var args = msgarray.Skip(1).ToArray();
-
-                        var argtext = msg.Replace(p + cmd + "", "");
-                        if (msg.Contains(p + cmd + " "))
-                            argtext = msg.Replace(p + cmd + " ", "");
-
                         if (cmd == "help")
                         {
                             string help = $"`{p}help` - lists the bot commands";
@@ -53,14 +69,19 @@ namespace stembote
                             string userinfo = $"`{p}userinfo` - displays info about yourself or a mentioned user";
                             string serverinfo = $"`{p}serverinfo` - displays info about the server";
                             string roll = $"`{p}roll [# of sides] [# of dice to roll]` - rolls some dice";
+                            string randomuser = $"`{p}randomuser` - selects a random user on the server";
 
-                            string cmds = $"\n{help}\n{usercount}\n{userinfo}\n{serverinfo}\n{roll}";
+                            string yt = $"`{p}yt [on/off]` - turn YT video notifications on/off";
+
+                            string cmds = $"\n{help}\n{usercount}\n{userinfo}\n{serverinfo}\n{roll}\n{randomuser}";
                             string greet = "Hi! I'm the STEM part of the HTC-Bote, a super-exclusive part only for the HTwins STEM server. ";
-                            string avacmds = $"I have a couple commands you can try out, which include: {cmds}";
+                            string avacmds = $"I have a couple commands you can try out, which include: {cmds}\nI also have some commands for use in private messages: \n{yt}";
 
                             string helpmsg = greet + avacmds;
                             await e.Channel.SendMessage(helpmsg);
                         }
+                        else if (cmd == "usercount")
+                            await e.Channel.SendMessage($"`{e.Server.Name}` currently has {e.Server.UserCount} users.");
                         else if (cmd == "userinfo")
                         {
                             users = e.Server.Users.OrderBy(user => user.JoinedAt).ToList(); // refresh user cache for later on
@@ -91,7 +112,7 @@ namespace stembote
                                 nickname = clearformatting(usr.Nickname);
 
                             string game;
-                            string status = usr.Status.ToString();
+                            // string status = usr.Status.ToString();
 
                             if (usr.CurrentGame == null)
                                 game = "[none]";
@@ -99,12 +120,14 @@ namespace stembote
                             {
                                 game = clearformatting(usr.CurrentGame.GetValueOrDefault().Name);
                                 string streamUrl = usr.CurrentGame.GetValueOrDefault().Url;
-                                if (usr.CurrentGame.GetValueOrDefault().Url != null)
-                                    status = $"streaming # {streamUrl}";
+                                //if (usr.CurrentGame.GetValueOrDefault().Url != null)
+                                //    status = $"streaming # {streamUrl}";
                             }
 
                             DateTime joined = usr.JoinedAt;
                             var joinedDays = DateTime.Now - joined;
+                            DateTimeOffset created = CreationDate(usr.Id);
+                            var createdDays = DateTime.Now - created;
                             string avatar = usr.AvatarUrl;
 
                             //	int usercount = 0;
@@ -127,6 +150,7 @@ namespace stembote
                                 $"[Current game]  {game}\n" +
                                 // $"[Status]        {status}\n" +
                                 $"[Joined]        {joined} ({joinedDays.Days} days ago)\n" +
+                                $"[Created]       {created.ToString().Replace(" +00:00", "")} ({createdDays.Days} days ago)\n" +
                                 //$"[Member #]      {memnum}\n" +      | broken for some reason. often shows users as being member #11
                                 $"[Avatar] {avatar}\n```");
                         }
@@ -137,7 +161,8 @@ namespace stembote
                             foreach (Role role in Roles)
                                 rolesList.Add(role.Name);
 
-                            var CreationDate = DateTime.Now - e.Server.Owner.JoinedAt;
+                            DateTimeOffset created = CreationDate(e.Server.Id);
+                            var createdDays = DateTime.Now - created;
                             string rolesString = string.Join(", ", rolesList.ToArray());
                             string region = e.Server.Region.Name;
 
@@ -150,7 +175,7 @@ namespace stembote
                                 $"[Role Count]      {e.Server.RoleCount}\n" +
                                 $"[Roles]           {clearformatting(rolesString)}\n" +
                                 $"[Owner]           @{clearformatting(e.Server.Owner.ToString())}\n" +
-                                $"[Creation date]   {e.Server.Owner.JoinedAt} ({CreationDate.Days} days ago) # possibly inaccurate\n" +
+							    $"[Created]         {created.ToString().Replace(" +00:00", "")} ({createdDays.Days} days ago)\n" +
                                 $"[Icon] {e.Server.IconUrl}\n" +
                                 $"```");
                         }
@@ -161,7 +186,7 @@ namespace stembote
                                 Random random = new Random(); // Generate some randomness
                                 string output = $"The 6-sided die rolled a {random.Next(1, 7)}."; // Define default output
 
-                                if (args.Length == 2) // tests if there are two arguments
+                                if (args.Count == 2) // tests if there are two arguments
                                 {
                                     if (args[1] != null)
                                     {
@@ -182,7 +207,7 @@ namespace stembote
                                                 while (rollTracker > 0) // If there are still dice to roll...
                                                 {
                                                     if (rollTracker > 1) /// ...and it's not only one die...
-				    									rolledDice += $"{random.Next(1, maxRolls)}, "; // ... add a random roll to the roledDice string
+                                                        rolledDice += $"{random.Next(1, maxRolls)}, "; // ... add a random roll to the roledDice string
                                                     else // or, if there is only one die left
                                                         rolledDice += $"and {random.Next(1, maxRolls)}"; // add the final random roll
 
@@ -197,7 +222,7 @@ namespace stembote
                                             output = "Woahh, that's a lot of dice to roll. Try lowering it below 30?"; // display error message because 30 rolls = lots of spam
                                     }
                                 }
-                                else if (args.Length == 1)
+                                else if (args.Count == 1)
                                 {
                                     if (args[0] != null)
                                     {
@@ -265,42 +290,49 @@ namespace stembote
                                     await e.Channel.SendMessage($"Something happened while trying to grab information about user #{rndNumber}.");
                             }
                         }
+                        else if (cmd == "debug" && isOwner)
+                        {
+                            var roles = e.Server.Roles.OrderBy(role => role.Position).ToList();
+                            var rolestring = "Sever roles: ";
+                            foreach (Role role in roles)
+                            {
+                                rolestring += role.Name + " | ";
+                            }
+                            Console.WriteLine(rolestring);
+                        }
                     }
                 }
-                else
+
+                bool isHSTEM = false;
+                if (e.Server != null)
+                    if (e.Server.Id == 282219466589208576)
+                        isHSTEM = true;
+
+                if (cmd == "yt" && (isHSTEM || e.Channel.IsPrivate))
                 {
-                    var msgarray = msg.Replace(p, "").Split(' ');
-                    string cmd = msgarray.FirstOrDefault().ToString();
-                    var args = msgarray.Skip(1).ToArray();
+                    if (_client.GetServer(282219466589208576).GetUser(e.User.Id) == null)
+                        await e.Channel.SendMessage($"You must be on HTwins STEM to use this command. You can join it here: https://discord.gg/4Gn4GAC");
 
-                    var argtext = msg.Replace(p + cmd + "", "");
-                    if (msg.Contains(p + cmd + " "))
-                        argtext = msg.Replace(p + cmd + " ", "");
-
-                    if (cmd == "yt")
+                    else if (args.Count == 1)
                     {
-						if(_client.GetServer(282219466589208576).GetUser(e.User.Id) == null)
-						    await e.Channel.SendMessage($"You must be on HTwins STEM to use this command. You can join it here: https://discord.gg/4Gn4GAC");
+                        var hstem = _client.GetServer(282219466589208576);
+                        var huser = hstem.GetUser(e.User.Id);
+                        var role = hstem.GetRole(289942717419749377);
 
-						else if (args.Length == 1)
-						{
-							var role = _client.GetServer(282219466589208576).FindRoles("Youtube").FirstOrDefault();
-
-							if(args[0] == "on")
-							{
-								await e.User.AddRoles(role);
-								await e.Channel.SendMessage($"You have been given the YouTube notification role on HTwins STEM.");
-							}
-							else if (args[0] == "off")
-							{
-								await e.User.RemoveRoles(role);
-								await e.Channel.SendMessage($"You have been removed from the YouTube notification role on HTwins STEM.")
-							}
-						}
-
-						else
-						    await e.Channel.SendMessage($"Proper usage: `{p}yt [on/off]`");
+                        if (args[0] == "on")
+                        {
+                            await huser.AddRoles(role);
+                            await e.Channel.SendMessage($"You have been given the YouTube notification role on HTwins STEM.");
+                        }
+                        else if (args[0] == "off")
+                        {
+                            await huser.RemoveRoles(role);
+                            await e.Channel.SendMessage($"You have been removed from the YouTube notification role on HTwins STEM.");
+                        }
                     }
+
+                    else
+                        await e.Channel.SendMessage($"Proper usage: `{p}yt [on/off]`");
                 }
             };
 
@@ -325,12 +357,124 @@ namespace stembote
 
         }
 
+        private static void caryVideoChecker(object source, ElapsedEventArgs e)
+        {
+            SyndicationResourceLoadSettings settings = new SyndicationResourceLoadSettings();
+            settings.RetrievalLimit = 1;
+    
+            Uri feedUrl = new Uri("https://www.youtube.com/feeds/videos.xml?user=carykh");
+            AtomFeed feed = AtomFeed.Create(feedUrl, settings);
+            var videos = feed.Entries;
+    
+            bool alreadyPosted = false;
+    
+            if (videos.Count() == 0)
+                Console.WriteLine("[Error] Feed contained no information.");
+    
+            foreach (var video in videos)
+            {
+	    		try
+	    		{
+	    			string videoUrlsFile = Directory.GetCurrentDirectory() + "/videoUrls.txt"; // String for the video URLS to check if a post is new, uses "videoUrls.txt" in directory the .exe is run from by default
+	    			var logFile = File.ReadAllLines(videoUrlsFile);
+	    			List<string> videoUrls = new List<string>(logFile);
+    
+	    			string newVideoUrl = video.Links.FirstOrDefault().Uri.ToString();
+	    			string videoTitle = video.Title.Content;
+    
+	    			foreach (var videoUrl in videoUrls)
+	    			{
+	    				if (newVideoUrl == videoUrl)
+	    				{
+	    					alreadyPosted = true;
+	    				}
+	    			}
+    
+	    			try
+	    			{
+	    				if (alreadyPosted == false)
+	    				{
+                            string vidinfo = $"\"{videoTitle}\" - {newVideoUrl}";
+
+	    					Console.WriteLine($"Found new video URL - {vidinfo} - Sending to discord");
+    
+	    					using (StreamWriter text = File.AppendText(videoUrlsFile))
+	    						text.WriteLine(newVideoUrl);
+    
+	    					_client.GetChannel(282225245761306624).SendMessage($"@here `carykh` has uploaded a new YouTube video!\n {vidinfo}");
+	    				}
+	    			}
+	    			catch (Exception error)
+	    			{
+	    				Console.WriteLine($"[Error] Bot ran into an issue while trying to post the video to discord. {error.ToString()}");
+	    			}
+	    		}
+	    		catch (Exception error)
+	    		{
+	    			Console.WriteLine("[Error] An error occured during the video check -" + error.ToString());
+	    		}
+            }
+    
+        }
+    
+        private static void abaVideoChecker(object source, ElapsedEventArgs e)
+        {
+            SyndicationResourceLoadSettings settings = new SyndicationResourceLoadSettings();
+            settings.RetrievalLimit = 1;
+    
+            Uri feedUrl = new Uri("https://www.youtube.com/feeds/videos.xml?user=1abacaba1");
+            AtomFeed feed = AtomFeed.Create(feedUrl, settings);
+            var videos = feed.Entries;
+    
+            bool alreadyPosted = false;
+    
+            if (videos.Count() == 0)
+                Console.WriteLine("[Error] Feed contained no information.");
+    
+            foreach (var video in videos)
+            {
+                string videoUrlsFile = Directory.GetCurrentDirectory() + "/videoUrls.txt"; // String for the video URLS to check if a post is new, uses "videoUrls.txt" in directory the .exe is run from by default
+                var logFile = File.ReadAllLines(videoUrlsFile);
+                List<string> videoUrls = new List<string>(logFile);
+    
+                string newVideoUrl = video.Links.FirstOrDefault().Uri.ToString();
+                string videoTitle = video.Title.Content;
+    
+                foreach (var videoUrl in videoUrls)
+                    if (newVideoUrl == videoUrl)
+                        alreadyPosted = true;
+    
+                try
+                {
+	    				if (alreadyPosted == false)
+	    				{
+                            string vidinfo = $"\"{videoTitle}\" - {newVideoUrl}";
+
+	    					Console.WriteLine($"Found new video URL - {vidinfo} - Sending to discord");
+    
+	    					using (StreamWriter text = File.AppendText(videoUrlsFile))
+	    						text.WriteLine(newVideoUrl);
+    
+	    					_client.GetChannel(282225245761306624).SendMessage($"@here `abacaba` has uploaded a new YouTube video!\n {vidinfo}");
+	    				}
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine($"[Error] Bot ran into an issue while trying to post the video to discord. {error.ToString()}");
+                }
+            }
+    
+        }
         public static string clearformatting(string input)
         {
             var output = "[empty string]";
             if (!string.IsNullOrWhiteSpace(input))
                 output = input.Replace("`", "​`").Replace("*", "​*").Replace("_", "​_").Replace("‮", " ");
             return output;
+        }
+        public static DateTimeOffset CreationDate(ulong id)
+        {
+			return DateTimeOffset.FromUnixTimeMilliseconds((Convert.ToInt64(id) >> 22) + 1420070400000);
         }
     }
 }
