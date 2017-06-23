9c2cd94e8e754e5f8d50ef0c91bedd7a0ad78bad
Bottersnike <>
2017-06-24 00:45:35 +0100

    gp form.

--- htstem_bote.py
+++ htstem_bote.py
@@ -372,7 +372,7 @@ I have a couple commands you can try out, which include:
                     print("GIT PULL!")
                     await self.client.send_message(message.channel, ":warning: Warning! The bot will not respond to messaes for up to a minute!")
                     res = subprocess.run("git pull", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
-                    await self.client.send_message(message.channel, "`Git` response: `{}`".format(res))
+                    await self.client.send_message(message.channel, "`Git` response: ```{}```".format(res))
                     await self.client.send_message(message.channel, ":white_check_mark: Crisis averted. I live again")
                     
             is_HSTEM = False
