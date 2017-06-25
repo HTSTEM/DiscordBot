8b0d7276a15464840367d1bdc11e4721c14f60e8
Eva Ilola <StuupieWhoopie@gmail.com>
2017-06-25 18:11:50 +0300

    Fix checks

--- cogs/util/checks.py
+++ cogs/util/checks.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 
 def is_staff():
     def predicate(ctx: commands.Context) -> bool:
+        # Ignore DMs
+        if ctx.guild is None:
+            return
         return ctx.author.permissions_in(ctx.guild.default_channel).manage_messages
     return commands.check(predicate)
 
@@ -12,6 +15,8 @@ def is_developer():
     def predicate(ctx: commands.Context) -> bool:
         ids = ctx.bot.config.get('ids', {})
         in_config = ctx.author.id in ids.get('developers', [])
-        has_role = ids.get('developer_role_id', 0) in map(lambda r: r.id, ctx.author.roles)
+        has_role = False
+        if ctx.guild is not None:
+            has_role = ids.get('developer_role_id', 0) in map(lambda r: r.id, ctx.author.roles)
         return in_config or has_role
     return commands.check(predicate)
