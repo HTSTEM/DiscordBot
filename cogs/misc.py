71cf76537ddd49dae9a81b5a99de0e649e0d7590
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-06-24 20:27:36 -0700

    Allow unlimited sides on a single die

--- cogs/misc.py
+++ cogs/misc.py
@@ -6,18 +6,18 @@ from discord.ext import commands
 
 class Misc:
     @commands.command()
-    async def roll(self, ctx, sides: int, number: int = 1):
+    async def roll(self, ctx, sides: int, how_many_dice: int = 1):
         '''Rolls a dice.'''
-        if sides < 1 or number < 1:
+        if sides < 1 or how_many_dice < 1:
             return await ctx.send('Yeah, sorry, but you can\'t roll something that doesn\'t exist.')
 
-        if sides > 100:
+        if sides > 100 and how_many_dice > 1:
             return await ctx.send('Woahh, that\'s a lot of sides. Try lowering it below 100?')
-        elif number > 30:
+        elif how_many_dice > 30:
             return await ctx.send('Woahh, that\'s a lot of dice to roll. Try lowering it below 30?')
 
         if sides == 1:
-            return await ctx.send('All {} of the 1-sided dice shocking rolled a 1.'.format(number))
+            return await ctx.send('All {} of the 1-sided dice shocking rolled a 1.'.format(how_many_dice))
 
         # easter eggs
         if sides == 666:
@@ -31,8 +31,8 @@ class Misc:
             return await ctx.send('{}? That\'s how many users are on the server! Well, your die rolled a {}, '
                                   'and according to the cache, that member is `{}`.'.format(sides, rolled + 1,
                                                                                             chosen.name))
-        result = ', '.join(str(random.randint(1, sides)) for i in range(number))
-        await ctx.send('The {} {}-sided {} rolled {}.'.format(number, sides, 'die' if number == 1 else 'dice', result))
+        result = ', '.join(str(random.randint(1, sides)) for i in range(how_many_dice))
+        await ctx.send('The {} {}-sided {} rolled {}.'.format(how_many_dice, sides, 'die' if how_many_dice == 1 else 'dice', result))
 
 
 def setup(bot):
