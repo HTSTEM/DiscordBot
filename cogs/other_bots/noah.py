77495d9a7b852c0e94bbc071047e73e521e59f1c
Bottersnike <bottersnike237@gmail.com>
2018-02-07 21:10:42 +0000

    hi

--- cogs/other_bots/noah.py
+++ cogs/other_bots/noah.py
@@ -11,12 +11,12 @@ class Hi:
         if message.guild is None:
             if 'hi' in message.content.lower():
                 c = self.bot.get_channel(364571585081901056)
-                e = discord.Embed(description=message.content)
+                embed = discord.Embed(description=message.content)
                 if message.embeds:
                     data = message.embeds[0]
                     if data.type == 'image':
                         embed.set_image(url=data.url)
-                
+
                 if message.attachments:
                     file_ = message.attachments[0]
                     if file_.url.lower().endswith(('png', 'jpeg', 'jpg', 'gif')):
@@ -27,10 +27,12 @@ class Hi:
                             value=f'[{file_.filename}]({file_.url})',
                             inline=False
                         )
-                        
+
                 embed.set_author(name=message.author.display_name,
                                  icon_url=message.author.avatar_url_as(format='png'))
                 embed.timestamp = message.created_at
+
+                await c.send(embed=embed)
 
 
 def setup(bot):
